59f51ec88cd876fe46f37c9552719df43846c610
yourunSpb <yourunSpb@gmail.com>
2015-11-03 08:02:50 +0300

    contract - addOrEdit method

--- cca-mgmt/cca-mgmt-service-war/src/main/java/ru/ccamgmt/service/CardCallApiImpl.java
+++ cca-mgmt/cca-mgmt-service-war/src/main/java/ru/ccamgmt/service/CardCallApiImpl.java
@@ -2,17 +2,20 @@ package ru.ccamgmt.service;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import ru.ccamgmt.contracts.AddCardCallRequest;
 import ru.ccamgmt.contracts.FormSectionsResponse;
+import ru.ccamgmt.contracts.MedicsResponse;
 import ru.ccamgmt.service.handler.CardCallHandler;
-import ru.ccamgmt.service.handler.UserHandler;
 import ru.ccamgmt.stubs.CardCallApi;
 import ru.ccamgmt.stubs.NotFoundException;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -29,14 +32,25 @@ public class CardCallApiImpl implements CardCallApi {
     @EJB
     CardCallHandler cardCallHandler;
 
+    @POST
+    @Path("/addOrEdit")
+    @ApiOperation(value = "add or update card call", notes = "", response = MedicsResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Response with actual data"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 404, message = "Not Found") })
+    @Override
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response cardCallAddOrEditPost(@ApiParam(value = "", required = true) AddCardCallRequest body) throws NotFoundException {
+        return null;
+    }
+
     @GET
     @Path("/formSections")
     @ApiOperation(value = "Get sections and questions", notes = "This method returns a list of questions and answers", response = FormSectionsResponse.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Response with actual data"),
-
             @ApiResponse(code = 400, message = "Bad request"),
-
             @ApiResponse(code = 404, message = "Not Found") })
     @Override
     @Produces(MediaType.APPLICATION_JSON)
