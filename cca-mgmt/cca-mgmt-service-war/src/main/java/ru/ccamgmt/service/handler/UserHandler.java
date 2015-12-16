c6c7d4c737c304bc35a485aa986643ec76d0e528
Musket89 <Carboon8@yandex.ru>
2015-12-16 23:18:44 +0300

    added method addMedicUser

--- cca-mgmt/cca-mgmt-service-war/src/main/java/ru/ccamgmt/service/handler/UserHandler.java
+++ cca-mgmt/cca-mgmt-service-war/src/main/java/ru/ccamgmt/service/handler/UserHandler.java
@@ -1,15 +1,18 @@
 package ru.ccamgmt.service.handler;
 
+import ru.ccamgmt.contracts.AddMedicUserRequest;
 import ru.ccamgmt.contracts.MedicDetails;
 import ru.ccamgmt.contracts.MedicsResponse;
 import ru.ccamgmt.domain.dao.MedicDAO;
 import ru.ccamgmt.domain.entity.medic.Medic;
+import ru.ccamgmt.domain.entity.medic.MedicPosition;
+import ru.ccamgmt.domain.entity.medic.MedicRole;
+import ru.ccamgmt.domain.entity.medic.MedicUser;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Yuriy Stolyarenko on 28.10.2015.
@@ -36,4 +39,36 @@ public class UserHandler {
         medicResponse.setMedics(medicDetailsList);
         return Response.ok(medicResponse).build();
     }
+
+    public Response addMedicUser(AddMedicUserRequest body) {
+        Medic medic = new Medic();
+
+
+        MedicUser medicUser = new MedicUser();
+        medicUser.setFirstName(body.getFirstName());
+        medicUser.setMiddleName(body.getMiddleName());
+        medicUser.setLastName(body.getLastName());
+        medicUser.seteMailAddress(body.getMailAddress());
+        medic.setMedicUser(medicUser);
+
+        MedicPosition position = new MedicPosition();
+        position.setId(body.getMedicPositionId());
+        medic.setMedicPosition(position);
+
+        Set<MedicRole> medicRoles = new HashSet<>();
+        for (Long roleId: body.getRoleList()) {
+            MedicRole role = new MedicRole();
+            role.setId(roleId);
+            medicRoles.add(role);
+        }
+        medic.setMedicRoles(medicRoles);
+
+        medicDAO.persist(medic);
+        return Response.ok().build();
+    }
+
+
+
+
+
 }
--- cca-mgmt/cca-mgmt-service-war/src/main/java/ru/ccamgmt/service/userApiImpl.java
+++ cca-mgmt/cca-mgmt-service-war/src/main/java/ru/ccamgmt/service/userApiImpl.java
@@ -1,9 +1,7 @@
 package ru.ccamgmt.service;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
+import ru.ccamgmt.contracts.AddMedicUserRequest;
 import ru.ccamgmt.contracts.MedicsResponse;
 import ru.ccamgmt.service.handler.UserHandler;
 import ru.ccamgmt.stubs.NotFoundException;
@@ -12,6 +10,7 @@ import ru.ccamgmt.stubs.UserApi;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -28,6 +27,24 @@ public class UserApiImpl implements UserApi {
     @EJB
     UserHandler userHandler;
 
+    @POST
+    @Path("/addMedicUser")
+
+
+    // MedicsResponse
+    @ApiOperation(value = "add or update MedicUser", notes = "", response = MedicsResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Response with actual data"),
+
+            @ApiResponse(code = 400, message = "Bad request"),
+
+            @ApiResponse(code = 404, message = "Not Found") })
+    @Override
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response userAddMedicUserPost(@ApiParam(value = "", required = true) AddMedicUserRequest body) throws NotFoundException {
+        return userHandler.addMedicUser(body);
+    }
+
     @GET
     @Path("/medicList")
     @ApiOperation(value = "Get all the medics at the substation", notes = "This method returns list of Medics", response = MedicsResponse.class)
@@ -37,8 +54,7 @@ public class UserApiImpl implements UserApi {
             @ApiResponse(code = 400, message = "Bad request"),
 
             @ApiResponse(code = 404, message = "Not Found") })
-
-    @Override
+   @Override
     @Produces(MediaType.APPLICATION_JSON)
     public Response userMedicListGet() throws NotFoundException {
         return userHandler.medicListHandle();
